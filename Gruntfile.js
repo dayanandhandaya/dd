da5011963cf769064d330df26862eaa735f151eb
User <user.push@hackerrank.workspace>
2020-12-09 16:30:55 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports=function(grunt){
+
+grunt.initConfig({
+  connect:{
+    server:{
+      option:{
+        port:8000,
+        protocol:"http",
+        hostname:"localhost,"
+        base:"."
+        directory:null,
+        open:true,
+        keepalive:true
+      }
+    }
+  }
+});
+grunt.loadNpmTasks('grunt-contrib-connect');
+};
